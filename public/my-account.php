f6af7754962c07a8fc65b2f00cc4e8edf04c76ca
Damian972 <hyoma972@gmail.com>
2019-10-22 18:38:46 -0400

    using orm

--- public/my-account.php
+++ public/my-account.php
@@ -2,10 +2,7 @@
 
 define('ROOT', dirname(__FILE__, 2));
 
-require(ROOT.'/config/config.php');
-require(INC.'/Database.php');
-require(INC.'/Smarty.class.php');
-require(INC.'/Utils.php');
+require(ROOT.'/vendor/autoload.php');
 
 # Start session
 session_start();
@@ -14,7 +11,7 @@ session_start();
 if (NEED_LOGIN && !Utils::isLogged()) header('Location: '.BASE_URL.'/login.php');
 if (!NEED_LOGIN) Utils::redirectToHome();
 
-$pdo = new Database();
+$db = Utils::getDatabase();
 
 # Change current user informations
 if (isset($_POST['change_my_account_info'])) {
@@ -28,28 +25,25 @@ if (isset($_POST['change_my_account_info'])) {
             if (empty($_SESSION['form_errors'])) {
                 if (empty($password) && empty($password_v)) {
                     // Save user info without password
-                    $is_query_ok = $pdo->query('UPDATE users SET username = :username, email = :email WHERE email = :old_email', [
+                    $is_query_ok = $db->update('users', [
                         'username' => htmlspecialchars($username),
-                        'email' => htmlspecialchars($email),
-                        'old_email' => $_SESSION['user']['email']
-                    ]);
+                        'email' => htmlspecialchars($email)
+                    ], ['email' => $_SESSION['user']['email']]);
                 } else {
                     // With password
-                    $is_query_ok = $pdo->query('UPDATE users SET username = :username, email = :email, password = :password WHERE email = :old_email', [
+                    $is_query_ok = $db->update('users', [
                         'username' => htmlspecialchars($username),
                         'email' => htmlspecialchars($email),
                         'password' => Utils::encryptData(htmlspecialchars($password)),
-                        'old_email' => $_SESSION['user']['email']
-                    ]);
+                    ], ['email' => $_SESSION['user']['email']]);
                 }
-                if ($is_query_ok) {
+                if ($is_query_ok !== false) {
                     echo 'yessssssss';
                     $_SESSION['user']['email'] = htmlspecialchars($email);
                     Utils::setFlash('Modifications enregistrées !!', 'success');
         
-                } else $_SESSION['form_errors']['email'] = 'Un erreur s\'est produite... Merci de contacter l\'administrateur.';
+                } else  Utils::setFlash('Un erreur s\'est produite... Merci de contacter l\'administrateur.', 'danger');
             }
-            
         } else $_SESSION['form_errors']['email'] = 'Merci de bien vouloir entrer un email valide.';
 
     } else $_SESSION['form_errors']['username'] = 'Votre nom d\'utilisateur doit être supérieur à 3 caractères';
@@ -60,7 +54,7 @@ if (isset($_POST['change_my_account_info'])) {
 }
 
 
-$user_account = $pdo->row('SELECT * FROM users WHERE email = :email', ['email' => $_SESSION['user']['email']]);
+$user_account = $db->get('users', ['username', 'email'], ['email' => $_SESSION['user']['email']]);
 if (!$user_account) {
     // Destroy session
     session_destroy();
