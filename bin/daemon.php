689965c130052688645aabaddb757619aeb7347a
Damian972 <hyoma972@gmail.com>
2019-11-04 16:23:29 -0400

    add daemon to populate the db with data retrieved from the sensor

--- bin/daemon.php
+++ bin/daemon.php
@@ -5,12 +5,12 @@ define('ROOT', dirname(__FILE__, 2));
 require(ROOT.'/vendor/autoload.php');
 
 $is_install = false;
-if (file_exists(VARF.'/'.DB_NAME.'.db')) $is_install = true;
+if (file_exists(VARF.'/.installed')) $is_install = true;
 
 try {
 	if (!$is_install) {
-		echo 'You need to install first, use "php alexia.php --install"'.PHP_EOL;
-		echo 'If not work, check you pdo driver or if you`\re using mysql create the database first'.PHP_EOL;
+		echo 'Merci de procéder à l\'installation avec "php alexia.php --install"'.PHP_EOL;
+		echo 'Si cela ne fonctionne pas, vérifier vos drivers pour mysql (slqlite or mysql) sinon vérifier si votre base de données est créée.'.PHP_EOL;
 		die();
 	}
 	$db = Utils::getDatabase();
@@ -24,12 +24,28 @@ try {
 
 			$data = ['temperature' => floatval($matches[0]), 'created_at' => date("Y-m-d H:i:s")];
 
-			if (isset($options['refresh_time_cli']) && isset($options['alert_threshold_min']) 
-				&& isset($options['alert_threshold_max']) && isset($options['alert_method'])) {
+			if (isset($options['refresh_time_cli']) && isset($options['alert_threshold_min_temperature']) 
+				&& isset($options['alert_threshold_max_temperature']) && isset($options['alert_method'])) {
 
-					if ()
-					
-			}
+				$alert = '';
+				if ($data['temperature'] <= intval($options['alert_threshold_min_temperature'])) $alert = 'low';
+				if ($data['temperature'] >= intval($options['alert_threshold_max_temperature'])) $alert = 'hight';
+				
+				//var_dump($alert);
+				if (!empty($alert)) {
+					if ('pushbullet' === $options['alert_method'] && !empty($options['alert_method_pushbullet_api_key'])) {
+						// send notif via pushbullet
+						Utils::sendPushbulletNotif('Temperature alert', 
+							('low' === $alert) ? 'Low': 'Hight',
+							$options['alert_method_pushbullet_api_key']
+						);
+					} else {
+						// send mail
+						$users_emails = $db->select('users', ['email']);
+						Utils::sendMail($users_emails, 'Temperature alert', 'emails/temperature-alert.view.tpl', compact('data', 'alert'));
+					}
+				}
+			} else echo '['.$data['created_at'].'] => Impossible de verifier si'.PHP_EOL;
 			$db->insert('data', $data);
 
 			echo '['.$data['created_at'].'] => '.$data['temperature'].'°C'.PHP_EOL;
